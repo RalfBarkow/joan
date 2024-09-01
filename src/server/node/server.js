562cf8341eb308934f6f7d3f40a6c5113db4ec4b
zach-planet-nine <zach@planetnine.app>
2024-09-01 07:13:16 -0700

    cors

--- src/server/node/server.js
+++ src/server/node/server.js
@@ -1,9 +1,11 @@
 import config from './config/local.js';
 import express from 'express';
+import cors from 'cors';
 import user from './src/user/user.js';
 import sessionless from 'sessionless-node';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -124,4 +126,4 @@ console.warn(err);
 
 app.listen(3004);
 
-console.log('server listening on port 3004');
+console.log('server listening for credentials on port 3004');
